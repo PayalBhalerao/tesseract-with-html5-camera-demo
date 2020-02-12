b6a5442e39ad88ae5faaac9b02071d4211207c9f
Payal Bhalerao <payal13.bhalerao@gmail.com>
2020-02-12 11:44:24 +0530

    Add tessract-wih-html5-camera pacakge

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import CameraTesseract from 'tesseract-with-html5-camera/dist/lib';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [text, setText] = useState('');
+
+  const handleTakePhoto = (text) => {
+      // Do stuff with recognized text...
+      console.log('Recognize text: ', text);
+      setText(text)
+    }
+
+    return (
+      <>
+        <p align="center"> Recognized Text - {text || '""'} </p>
+        <CameraTesseract
+          onTextRecognize = { (text) => { handleTakePhoto(text); } }
+        />
+      </>
+    );
 }
 
 export default App;
