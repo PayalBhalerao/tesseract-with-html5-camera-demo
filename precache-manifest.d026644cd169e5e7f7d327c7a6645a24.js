d439cff094f3ce40c3c20ab5c3fb084e44367cb2
Payal Bhalerao <payal13.bhalerao@gmail.com>
2020-02-12 12:30:55 +0530

    Updates

---
+++ precache-manifest.d026644cd169e5e7f7d327c7a6645a24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b652eab9cc817d26031f0ea3e8712d4f",
+    "url": "/tesseract-with-html5-camera-demo/index.html"
+  },
+  {
+    "revision": "d3e1f8443e51729540c1",
+    "url": "/tesseract-with-html5-camera-demo/static/css/2.f0405d8a.chunk.css"
+  },
+  {
+    "revision": "f4598014f2e4c2a3c9fd",
+    "url": "/tesseract-with-html5-camera-demo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d3e1f8443e51729540c1",
+    "url": "/tesseract-with-html5-camera-demo/static/js/2.8c1b3398.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/tesseract-with-html5-camera-demo/static/js/2.8c1b3398.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4598014f2e4c2a3c9fd",
+    "url": "/tesseract-with-html5-camera-demo/static/js/main.13441c91.chunk.js"
+  },
+  {
+    "revision": "66ec269f753c01955029",
+    "url": "/tesseract-with-html5-camera-demo/static/js/runtime-main.fddf552b.js"
+  }
+]);
